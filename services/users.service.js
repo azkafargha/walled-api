8737afc4209fadf8244e6718e911cfad5fb944ed
azkafargha <azkafarghaa@gmail.com>
2024-12-18 15:02:52 +0700

    ganti bcrypt

--- services/users.service.js
+++ services/users.service.js
@@ -1,5 +1,5 @@
 const userRepository = require("../repositories/users.repository");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { generateAccessToken } = require("../utils/auth.util");
 
 const regist = async (userData) => {
