98a6ad9c6dd8ce4f863745adb0e3cbbc9b902ba9
azkafargha <azkafarghaa@gmail.com>
2024-12-18 14:17:00 +0700

    connect to vercel

--- db/db.js
+++ db/db.js
@@ -1,10 +1,6 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "db_walled",
-  password: "admin",
-  port: 5432,
+  connectionString: process.env.DB_URL,
 });
 
 module.exports = pool;
